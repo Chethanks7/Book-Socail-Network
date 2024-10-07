2f53589c7c86d03b99b4fa741422ebb2274d5295
Chethan <chethanks340@gmail.com>
2024-10-07 13:38:42 +0530

    created authentication provider with password encoder

---
+++ src/main/java/com/Book_social_Network/config/BeanConfig.java
@@ -0,0 +1,37 @@
+package com.Book_social_Network.config;
+
+import com.Book_social_Network.security.UserDetailsService;
+import com.Book_social_Network.security.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class BeanConfig {
+
+    private final UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+
+        return provider;
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+}
