62b1995884b7139d6b32d499faba190c914a80fa
Chethan <chethanks340@gmail.com>
2024-10-07 13:43:03 +0530

    loading the username from the database

---
+++ src/main/java/com/Book_social_Network/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.Book_social_Network.security;
+
+import com.Book_social_Network.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with userEmail: " + userEmail));
+    }
+}
