581525e0313990630e8e36a3cc1d07a19cab9828
Chethan <chethanks340@gmail.com>
2024-10-15 17:11:00 +0530

    to give response for client

---
+++ src/main/java/com/Book_social_Network/auth/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.Book_social_Network.auth;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+public class AuthenticationResponse {
+
+    private String token;
+
+}
