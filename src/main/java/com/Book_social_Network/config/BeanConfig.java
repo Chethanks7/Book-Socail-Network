007797dd14f1835719136d7fc59da6848ad5d827
Chethan <chethanks340@gmail.com>
2024-10-15 17:12:34 +0530

    created a bean for authentication manager

--- src/main/java/com/Book_social_Network/config/BeanConfig.java
+++ src/main/java/com/Book_social_Network/config/BeanConfig.java
@@ -5,8 +5,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -48,6 +51,11 @@ public class BeanConfig {
     public PasswordEncoder passwordEncoder() {
 
         return new BCryptPasswordEncoder(); // Returns a new instance of BCryptPasswordEncoder for password hashing.
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
 
